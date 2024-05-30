0d070acbdf5ce107e27c3de920034e2e918df997
FerBendalaFXS <fbendala@fxstreet.com>
2024-05-30 17:38:16 +0200

    structuring project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,61 @@
-module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+const eslint_config = {
+	env: {
+		browser: true,
+		es2024: true,
+		node: true,
+		jest: true
+	},
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'eslint:recommended',
+		'plugin:@tanstack/eslint-plugin-query/recommended',
+		'plugin:tailwindcss/recommended'
+	],
+	overrides: [
+		{
+			files: [
+				'**/__tests__/**/*.[jt]s?(x)',
+				'**/?(*.)+(spec|test).[jt]s?(x)'
+			],
+			extends: ['plugin:testing-library/react']
+		}
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: ['react', '@typescript-eslint', '@tanstack/query', 'tailwindcss'],
+	rules: {
+		indent: ['error', 'tab', { SwitchCase: 1 }],
+		'linebreak-style': 0,
+		quotes: ['error', 'single', { avoidEscape: true }],
+		semi: ['error', 'always'],
+		eqeqeq: 'error',
+		'no-trailing-spaces': 'error',
+		'object-curly-spacing': ['error', 'always'],
+		'arrow-spacing': [
+			'error',
+			{
+				before: true,
+				after: true
+			}
+		],
+		'no-console': 1,
+		'react/react-in-jsx-scope': 'off',
+		'global-require': 'off',
+		'no-case-declarations': 'off',
+		'@typescript-eslint/no-var-requires': 'off',
+		'@typescript-eslint/no-unused-vars': ['error'],
+		'@typescript-eslint/no-explicit-any': 'warn',
+		'no-unused-vars': 'warn',
+		'jsx-quotes': ['error', 'prefer-single']
+	},
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	}
+};
+
+module.exports = eslint_config;
