9e77508c1ceef845cadaae59329e0cc9ab20d6f7
FerBendalaFXS <fbendala@fxstreet.com>
2024-05-30 17:38:28 +0200

    structuring project

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	assetsInclude: ['./src/assets/scss/*.scss']
+	assetsInclude: ['./src/assets/scss/*.scss'],
+	css: {
+		loaderOptions: {
+			sass: { additionalData: ` @import "@/style/index.scss"; ` }
+		}
+	}
 });
